d4c63b24bec2f141305ca2e0e6dc745d88c2a565
Jeff <jpvalim@hotmail.com>
2025-12-28 19:35:01 -0300

    Iniciando recursos para servico

---
+++ backend/src/main/java/com/jpv/reservai/dto/ServicoNewDTO.java
@@ -0,0 +1,21 @@
+package com.jpv.reservai.dto;
+
+import com.jpv.reservai.entities.Servico;
+
+
+public class ServicoNewDTO {
+
+	private Long Codigo;
+	private String descricao;
+	private Double preco;
+	private String descricaoUnidade;
+	private Long tempoMinutos;
+	
+	
+		
+	public ServicoNewDTO() {}
+		
+	public ServicoNewDTO(Servico obj) {
+		
+	}
+}
